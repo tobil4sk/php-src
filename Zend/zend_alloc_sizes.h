82c96d5b3e5279e4b956523b1efdfaa3996014c8
Tobi Laskowski <tl621@cam.ac.uk>
2025-11-05 14:17:11 +0000

    Adjust allocation sizes for purecap cheri
    In cheri, all allocations have to be aligned to 16 bytes, so the smaller
    allocation sizes must be rounded up.

--- Zend/zend_alloc_sizes.h
+++ Zend/zend_alloc_sizes.h
@@ -24,12 +24,50 @@
 #define ZEND_MM_PAGES      (ZEND_MM_CHUNK_SIZE / ZEND_MM_PAGE_SIZE)  /* 512 */
 #define ZEND_MM_FIRST_PAGE (1)
 
-#define ZEND_MM_MIN_SMALL_SIZE		8
+#ifdef __CHERI_PURE_CAPABILITY__
+# define ZEND_MM_MIN_SMALL_SIZE		16
+#else
+# define ZEND_MM_MIN_SMALL_SIZE		8
+#endif
+
 #define ZEND_MM_MAX_SMALL_SIZE      3072
 #define ZEND_MM_MAX_LARGE_SIZE      (ZEND_MM_CHUNK_SIZE - (ZEND_MM_PAGE_SIZE * ZEND_MM_FIRST_PAGE))
 
 /* num, size, count, pages */
-#define ZEND_MM_BINS_INFO(_, x, y) \
+#ifdef __CHERI_PURE_CAPABILITY__
+# define ZEND_MM_BINS_INFO(_, x, y) \
+	_( 0,   16,  256, 1, x, y) \
+	_( 1,   16,  256, 1, x, y) \
+	_( 2,   32,  256, 1, x, y) \
+	_( 3,   32,  128, 1, x, y) \
+	_( 4,   48,   85, 1, x, y) \
+	_( 5,   48,   85, 1, x, y) \
+	_( 6,   64,   64, 1, x, y) \
+	_( 7,   64,   64, 1, x, y) \
+	_( 8,   80,   51, 1, x, y) \
+	_( 9,   96,   42, 1, x, y) \
+	_(10,  112,   36, 1, x, y) \
+	_(11,  128,   32, 1, x, y) \
+	_(12,  160,   25, 1, x, y) \
+	_(13,  192,   21, 1, x, y) \
+	_(14,  224,   18, 1, x, y) \
+	_(15,  256,   16, 1, x, y) \
+	_(16,  320,   64, 5, x, y) \
+	_(17,  384,   32, 3, x, y) \
+	_(18,  448,    9, 1, x, y) \
+	_(19,  512,    8, 1, x, y) \
+	_(20,  640,   32, 5, x, y) \
+	_(21,  768,   16, 3, x, y) \
+	_(22,  896,    9, 2, x, y) \
+	_(23, 1024,    8, 2, x, y) \
+	_(24, 1280,   16, 5, x, y) \
+	_(25, 1536,    8, 3, x, y) \
+	_(26, 1792,   16, 7, x, y) \
+	_(27, 2048,    8, 4, x, y) \
+	_(28, 2560,    8, 5, x, y) \
+	_(29, 3072,    4, 3, x, y)
+#else
+# define ZEND_MM_BINS_INFO(_, x, y) \
 	_( 0,    8,  512, 1, x, y) \
 	_( 1,   16,  256, 1, x, y) \
 	_( 2,   24,  170, 1, x, y) \
@@ -60,5 +98,6 @@
 	_(27, 2048,    8, 4, x, y) \
 	_(28, 2560,    8, 5, x, y) \
 	_(29, 3072,    4, 3, x, y)
+#endif
 
 #endif /* ZEND_ALLOC_SIZES_H */
